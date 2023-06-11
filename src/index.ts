2bee22aefbe39a8f8025a6ccf77eda5c9a574ff5
razercreedunity <iwanroslan35@gmail.com>
2023-06-11 12:58:13 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+app.use(cors({
+  origin: 'https://freelancers-application.vercel.app/',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  methods: 'GET, POST, PUT, DELETE, PATCH',
+  credentials: true
+}));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
