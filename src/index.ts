9c8758a0eac398781f4adcc33898459b632125f8
razercreedunity <iwanroslan35@gmail.com>
2023-06-11 13:02:31 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,10 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(cors({
   origin: 'https://freelancers-application.vercel.app/',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  methods: 'GET, POST, PUT, DELETE, PATCH',
+  methods: 'GET, POST, PUT, DELETE, PATCH, GETALL',
   credentials: true
 }));
 app.use(compression());
